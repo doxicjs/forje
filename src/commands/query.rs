fb2b8d4de4211dd28e1f1deff8abd074d2093b43
2khan <thejargalsaikhan@gmail.com>
2025-03-12 03:36:01 +0800

    fix args

--- src/commands/query.rs
+++ src/commands/query.rs
@@ -1,5 +1,5 @@
 use crate::services::template_engine::render_template;
-use clap::{Arg, ArgAction, ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
 use tera::Context;
 
 pub fn cmd() -> clap::Command {
@@ -7,8 +7,6 @@ pub fn cmd() -> clap::Command {
     .about("Forges Queries using Tanstack Query\nForges use{Name}.tsx file that exports the query")
     .arg(
       Arg::new("query_name")
-        .short('n')
-        .long("name")
         .required(true)
         .help("Hammers the name of the query"),
     )
@@ -23,14 +21,6 @@ pub fn cmd() -> clap::Command {
         .short('p')
         .long("params")
         .help("Hammers the query with Params Zod schema"),
-    )
-    .arg(
-      Arg::new("is_enabled")
-        .short('e')
-        .long("enabled")
-        .action(ArgAction::SetTrue)
-        .requires("route_name")
-        .help("Hammers the query to only run when Route is provided"),
     );
 }
 
@@ -64,10 +54,6 @@ pub fn exec(matches: Option<&ArgMatches>) {
   if params_name.is_some() {
     context.insert("params_name", &params_name.unwrap().to_string());
   }
-
-  let is_enabled = matches.unwrap().get_one::<bool>("is_enabled").unwrap();
-
-  context.insert("is_enabled", &is_enabled);
 
   context.insert("query_name", &query_name.to_string());
 
